2ad6ca3f32daa41002d3e17fe3e0564a4becfce5
MariahArvanitakis <mariah.arvani@gmail.com>
2017-05-01 22:44:17 +1000

    ideas

--- project3ideas/assets/js/main.js
+++ project3ideas/assets/js/main.js
@@ -1,24 +1,6 @@
 $(document).ready(function() {
 
-  $('.number').mouseenter(function() {
-    $('.footnote').addClass('showme');
-  });
-
-  $('.number').mouseleave(function() {
-    $('.footnote').removeClass('showme');
-  });
-
-
-
-
-  $('.blueheader').mouseenter(function() {
-    $('.redheader').addClass('hide');
-  });
-
-  $('.blueheader').mouseleave(function() {
-    $('.redheader').removeClass('hide');
-  });
-
+  
 
 
 
