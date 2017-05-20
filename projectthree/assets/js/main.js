87f233ab859c84a032f4284fe145e9c289da4ed7
MariahArvanitakis <mariah.arvani@gmail.com>
2017-05-20 12:56:38 +1000

    java

--- projectthree/assets/js/main.js
+++ projectthree/assets/js/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
 
-  
+
+    $(".apple").mouseenter(function() {
+      $(".apple__image").addClass("showme");
+    });
+
+    $(".apple").mouseleave(function() {
+      $(".apple__image").removeClass("showme");
+    });
+
+
+
+
+
+
+
 
 
 
