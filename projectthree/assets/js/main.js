083adf1e3e341f67cf6f6fab57e4260b43fec242
MariahArvanitakis <mariah.arvani@gmail.com>
2017-05-20 15:52:52 +1000

    applying rollover

--- projectthree/assets/js/main.js
+++ projectthree/assets/js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
 
+    $(".dior").mouseenter(function() {
+      $(".dior__image").addClass("showme");
+    });
+
+    $(".dior").mouseleave(function() {
+      $(".dior__image").removeClass("showme");
+    });
+
+
+
+
+
     $(".apple").mouseenter(function() {
       $(".apple__image").addClass("showme");
     });
@@ -12,6 +24,28 @@ $(document).ready(function() {
 
 
 
+
+    $(".comme").mouseenter(function() {
+      $(".comme__image").addClass("showme");
+    });
+
+    $(".comme").mouseleave(function() {
+      $(".comme__image").removeClass("showme");
+    });
+
+
+
+
+
+
+
+    $(".nespresso").mouseenter(function() {
+      $(".nespresso__image").addClass("showme");
+    });
+
+    $(".nespresso").mouseleave(function() {
+      $(".nespresso__image").removeClass("showme");
+    });
 
 
 
