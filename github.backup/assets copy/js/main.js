2b1e7f85979b39419ce492aa0f4cde0459429740
MariahArvanitakis <mariah.arvani@gmail.com>
2017-05-23 10:43:20 +1000

    relink

---
+++ github.backup/assets copy/js/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+
+    $(".dior").mouseenter(function() {
+      $(".dior__image").addClass("showme");
+    });
+
+    $(".dior").mouseleave(function() {
+      $(".dior__image").removeClass("showme");
+    });
+
+
+
+
+
+    $(".apple").mouseenter(function() {
+      $(".apple__image").addClass("showme");
+    });
+
+    $(".apple").mouseleave(function() {
+      $(".apple__image").removeClass("showme");
+    });
+
+
+
+    $(".euro").mouseenter(function() {
+      $(".euro__image").addClass("showme");
+    });
+
+    $(".euro").mouseleave(function() {
+      $(".euro__image").removeClass("showme");
+    });
+
+
+
+
+    $(".comme").mouseenter(function() {
+      $(".comme__image").addClass("showme");
+    });
+
+    $(".comme").mouseleave(function() {
+      $(".comme__image").removeClass("showme");
+    });
+
+
+
+
+
+
+
+    $(".nespresso").mouseenter(function() {
+      $(".nespresso__image").addClass("showme");
+    });
+
+    $(".nespresso").mouseleave(function() {
+      $(".nespresso__image").removeClass("showme");
+    });
+
+
+
+    $('.nav').click(function() {
+      $('.menu').toggleClass('showme');
+    });
+
+
+
+
+        $('.navtwo').click(function() {
+          $('.menutwo').toggleClass('showme');
+        });
+
+});
